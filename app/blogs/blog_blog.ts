d664415130d13eebe64bf48971d5b0eedfcd969b
ellrussbest <ellrussbest@gmail.com>
2024-11-17 16:00:43 +0300

    Added the first Blog

--- app/blogs/blog_blog.ts
+++ app/blogs/blog_blog.ts
@@ -8,10 +8,10 @@ interface Iblogs {
 }
 
 const blogs: Iblogs = {
-  // "01": {
-  //   url: "https://reasonforcpp.blogspot.com/2024/11/why-you-should-always-consider-c.html",
-  //   description: "Why you should always consider C++",
-  // },
+  "01": {
+    url: "https://reasonforcpp.blogspot.com/2024/11/why-you-should-always-consider-c.html",
+    description: "Overloading the C++ std::ostream (<<) operator",
+  },
 };
 
 export default blogs;
