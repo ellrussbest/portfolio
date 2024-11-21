f0bfb75f64addf6407bc00d9e655fbd97ea84941
ellrussbest <ellrussbest@gmail.com>
2024-11-22 01:29:45 +0300

    removed opacity from profile

--- components/photo/index.tsx
+++ components/photo/index.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Photo() {
   return (
     <div className="w-full h-full relative flex items-center justify-center">
-      <div className="xd:w-[298px] xd:h-[298px] w-[498px] h-[498px] mix-blend-lighten opacity-50 overflow-hidden rounded-full relative border-2 border-[#f29871] border-dotted">
+      <div className="xd:w-[298px] xd:h-[298px] w-[498px] h-[498px] overflow-hidden rounded-full relative border-2 border-[#f29871] border-dotted">
         <Image
           src="/images/profile.png"
           priority
