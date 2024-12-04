b8fcec692ce4e9833a4e51969ed45ddf047ff5d1
ellrussbest <ellrussbest@gmail.com>
2024-12-04 20:10:00 +0300

    changed profile

--- components/photo/index.tsx
+++ components/photo/index.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Photo() {
   return (
     <div className="w-full h-full relative flex items-center justify-center">
-      <div className="xd:w-[298px] xd:h-[298px] w-[498px] h-[498px] overflow-hidden rounded-full relative xd:border-x-8 xl:border-y-[10px] border-[rgba(255,77,77,.2)] border-dotted">
+      <div className="xd:w-[298px] xd:h-[298px] w-[498px] h-[498px] overflow-hidden rounded-full relative border-8 xd:border-[6px] border-[rgba(255,77,77,.5)] border-dotted bg-white/5">
         <Image
           src="/images/profile.png"
           priority
