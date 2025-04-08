a156e0b0879ed698dd02ea1ce115f3865ffaf368
ellrussbest <ellrussbest@gmail.com>
2025-04-08 21:09:00 +0300

    changed the profile

--- components/photo/index.tsx
+++ components/photo/index.tsx
@@ -5,12 +5,12 @@ export default function Photo() {
     <div className="w-full h-full relative flex items-center justify-center">
       <div className="xd:w-[298px] xd:h-[298px] w-[498px] h-[498px] overflow-hidden rounded-full relative border-8 xd:border-[6px] border-[rgba(255,77,77,.5)] border-dotted bg-white/5">
         <Image
-          src="/images/profile.png"
+          src="/images/profile.jpg"
           priority
           quality={100}
-          fill
-          alt=""
-          className="object-cover"
+          width={853}
+          height={1280}
+          alt="Profile Image"
         />
       </div>
     </div>
